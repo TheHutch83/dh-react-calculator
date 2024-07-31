275688fe74f3ad258ac9f11bf11c10acfde81253
David Hutcheson <davidhutcheson@macbook-air.lan>
2024-07-31 17:58:44 +0100

    updates before restarting the app

--- src/App.js
+++ src/App.js
@@ -1,50 +1,44 @@
 import React from "react";
 import Button from "./components/Button";
 import "./styling/App.css";
+import "./styling/Button.css";
 import Input from "./components/Input";
 import Info from "./components/Info";
 
 const App = () => {
   return (
-    <div className="app">
-      <div className="calculator">
-        <div>
+    <div className="app-container">
+      <div className="calculator-container">
+        <div className="input">
           <Input />
         </div>
         <div className="info-panel">
           <Info />
         </div>
         <div className="solar-panel"></div>
-        <div className="calc-row">
+        <div className="calc-grid">
           <Button symbol={"%"} />
           <Button symbol={"7"} />
           <Button symbol={"8"} />
           <Button symbol={"9"} />
           <Button symbol={"x"} />
           <Button symbol={"÷"} />
-        </div>
-        <div className="calc-row">
           <Button symbol={"√"} />
           <Button symbol={"4"} />
           <Button symbol={"5"} />
           <Button symbol={"6"} />
           <Button symbol={"-"} />
           <Button symbol={"MR"} />
-        </div>
-        <div className="calc-row">
           <Button symbol={"C"} />
           <Button symbol={"1"} />
           <Button symbol={"2"} />
           <Button symbol={"3"} />
-          <Button symbol={"+"} />
+          <Button symbol={"+"} className="large-button" />
           <Button symbol={"M-"} />
-        </div>
-        <div className="calc-row">
           <Button symbol={"AC"} />
           <Button symbol={"0"} />
           <Button symbol={"."} />
           <Button symbol={"="} />
-          <Button symbol={"+"} />
           <Button symbol={"M+"} />
         </div>
       </div>
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,19 @@
 import "../styling/Button.css";
 
-const Button = ({ symbol }) => {
-  return <div className="normal-buttons">{symbol}</div>;
+const Button = ({ symbol, className }) => {
+  const memoryButtonClass = ["MR", "M+", "M-"].includes(symbol)
+    ? "memory-button"
+    : "";
+
+  const cancelButtonClass = ["AC", "C"].includes(symbol) ? "cancel-button" : "";
+
+  return (
+    <div
+      className={`normal-buttons ${memoryButtonClass} ${cancelButtonClass} ${className}`}
+    >
+      {symbol}
+    </div>
+  );
 };
 
 export default Button;
