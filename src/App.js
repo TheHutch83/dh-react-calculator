bb4c8a1801ec56fedfd1c06c4e40f9929d9c4db2
David Hutcheson <d.hutcheson@hotmail.co.uk>
2024-08-12 10:52:36 +0100

    back to original code to de-bug

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import Button from "./components/Button";
 import "./styling/App.css";
 import Input from "./components/Input";
@@ -6,98 +6,6 @@ import Info from "./components/Info";
 import Solar from "./components/Solar";
 
 const App = () => {
-  const [input, setInput] = useState("");
-  const [result, setResult] = useState("");
-
-  const handleButtonClick = (symbol) => {
-    if (symbol === "=") {
-      calculateResult();
-    } else if (symbol === "AC") {
-      clearInput();
-    } else if (symbol === "C") {
-      deleteLast();
-    } else if (symbol === "x") {
-      setInput((prevInput) => prevInput + "*");
-    } else if (symbol === "÷") {
-      setInput((prevInput) => prevInput + "/");
-    } else {
-      setInput((prevInput) => prevInput + symbol);
-    }
-  };
-
-  const calculateResult = () => {
-    try {
-      const evaulatedResult = evaluateExpression(input);
-      setResult(evaulatedResult.toString());
-    } catch (error) {
-      setResult("error");
-    }
-  };
-
-  const clearInput = () => {
-    setInput("");
-    setResult("");
-  };
-
-  const deleteLast = () => {
-    setInput((prevInput) => prevInput.slice(0, -1));
-  };
-
-  const evaluateExpression = (expression) => {
-    const operators = {
-      "+": (a, b) => a + b,
-      "-": (a, b) => a - b,
-      "*": (a, b) => a * b,
-      "/": (a, b) => a / b,
-    };
-
-    const precedence = {
-      "+": 1,
-      "-": 1,
-      "*": 2,
-      "/": 2,
-    };
-
-    const outputQueue = [];
-    const operatorStack = [];
-
-    const tokens = expression.match(/\d+|\+|\-|\*|\//g);
-
-    tokens.forEach((token) => {
-      if (!isNaN(token)) {
-        outputQueue.push(Number(token));
-      } else if (operators[token]) {
-        while (
-          operatorStack.length &&
-          precedence[operatorStack[operatorStack.length - 1]] >=
-            precedence[token]
-        ) {
-          outputQueue.push(operatorStack.pop());
-        }
-        operatorStack.push(token);
-      }
-    });
-
-    while (operatorStack.length) {
-      outputQueue.push(operatorStack.pop());
-    }
-
-    const evaluationStack = [];
-
-    outputQueue.forEach((token) => {
-      if (typeof token === "number") {
-        evaluationStack.push(token);
-      } else if (operators[token]) {
-        const b = evaluationStack.pop();
-        const a = evaluationStack.pop();
-        const result = operators[token](a, b);
-        evaluationStack.push(result);
-      }
-    });
-
-    return evaluationStack[0];
-  };
-
   return (
     <div className="app-container">
       <div className="calculator-container">
@@ -110,38 +18,29 @@ const App = () => {
         <div className="solar-panel"></div>
         <Solar />
         <div className="calc-grid">
-          {[
-            "%",
-            "7",
-            "8",
-            "9",
-            "x",
-            "÷",
-            "√",
-            "4",
-            "5",
-            "6",
-            "-",
-            "MR",
-            "C",
-            "1",
-            "2",
-            "3",
-            "+",
-            "M-",
-            "AC",
-            "0",
-            ".",
-            "=",
-            "M+",
-          ].map((symbol) => (
-            <Button
-              key={symbol}
-              symbol={symbol}
-              onClick={() => handleButtonClick(symbol)}
-              className={symbol === "+" ? "large-button" : ""}
-            />
-          ))}
+          <Button symbol={"%"} />
+          <Button symbol={"7"} />
+          <Button symbol={"8"} />
+          <Button symbol={"9"} />
+          <Button symbol={"x"} />
+          <Button symbol={"÷"} />
+          <Button symbol={"√"} />
+          <Button symbol={"4"} />
+          <Button symbol={"5"} />
+          <Button symbol={"6"} />
+          <Button symbol={"-"} />
+          <Button symbol={"MR"} />
+          <Button symbol={"C"} />
+          <Button symbol={"1"} />
+          <Button symbol={"2"} />
+          <Button symbol={"3"} />
+          <Button symbol={"+"} className="large-button" />
+          <Button symbol={"M-"} />
+          <Button symbol={"AC"} />
+          <Button symbol={"0"} />
+          <Button symbol={"."} />
+          <Button symbol={"="} />
+          <Button symbol={"M+"} />
         </div>
       </div>
     </div>
